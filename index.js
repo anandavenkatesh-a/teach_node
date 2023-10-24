a4f8288780aff5937dd4055c452abe87e2de3323
comp21823 <blackyvenky21@gmail.com>
2023-10-24 14:18:02 +0530

    server created

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+var cors = require('cors');
+
+const app = express();
+
+// Neccessary Middlewares
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+app.use(
+  cors({
+    origin: ["http://localhost:2000"],
+    credentials: true,
+  }),
+);
+
+const mongoString = "mongodb://localhost:27017/learn_node"
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+const dataSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    rollno: {
+        required: true,
+        type: Number
+    }
+})
+
+const Student = mongoose.model('Student', dataSchema)
+
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    return res.status(200).json({
+        message: "welcome!"
+    });
+})
+
+app.use("/", router);
+
+//Post Method
+router.post('/addStudent', async (req, res) => {
+    try {
+        console.log(req.body);
+        const new_student = await Student.create({
+            name: req.body.name,
+            rollno: req.body.rollno
+        })
+
+        res.status(200).json(new_student)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+//Get Method
+router.get('/getAll', async (req, res) => {
+    try {
+        const data = await Student.find();
+        res.json(data)
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
